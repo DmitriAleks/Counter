526e27121c60854cfe896abc155046abb9bc0918
dima <Dima.aleks.94@mail.ru>
2021-07-03 21:10:52 +0300

    new case reducer Change value

--- Counter/src/store/counter-reducer.ts
+++ Counter/src/store/counter-reducer.ts
@@ -1,12 +1,19 @@
 const ADD_PLUS_NUMBER = 'ADD-PLUS-NUMBER'
 const UPDATE_NUMBER = 'UPDATE-NUMBER'
-type ActionUnionType = RemoveTaskAT|UpdateNumberAT
+const CHANGE_VALUE = 'CHANGE-VALUE'
+
+type ActionUnionType = RemoveTaskAT|UpdateNumberAT|ChangeValueAT
 
 type RemoveTaskAT = {
     type: 'ADD-PLUS-NUMBER',
 }
 type UpdateNumberAT = {
         type: 'UPDATE-NUMBER',
+}
+type ChangeValueAT = {
+    type: 'CHANGE-VALUE',
+    minNumber:number,
+    maxNumber: number,
 }
 
 export type initialStateType ={
@@ -45,6 +52,16 @@ export const counterReducer = (state = initialState, action: ActionUnionType):in
                 numberTable: state.numberTable = state.startValue,
                 statusInc: state.statusInc = false
             }
+        case CHANGE_VALUE:
+            return {
+                ...state,
+                startValue: state.startValue = action.minNumber,
+                valueSettings: state.valueSettings = action.maxNumber,
+                numberTable: state.numberTable = action.minNumber,
+                error: action.maxNumber <= action.minNumber || action.minNumber < 0,
+                errorMessage: state.errorMessage = "Ошибка ввода"
+
+            }
         default:
             return state
     }
@@ -54,4 +71,11 @@ export const addPlusNumberAC = ():RemoveTaskAT => {
 }
 export const updateNumberAC = ():UpdateNumberAT => {
     return {type: 'UPDATE-NUMBER'}
+}
+export const changeValueAC = (minNumber: number, maxNumber: number):ChangeValueAT => {
+    return {type: 'CHANGE-VALUE',
+        minNumber,
+        maxNumber,
+
+    }
 }
